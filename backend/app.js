53389ab67d830e8211fe8047b58746f57e6175e8
noumedem tami florian <noumedemtamiflorian@gmail.com>
2023-01-24 05:34:02 +0100

    Configurez votre base de données

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
+
+mongoose.connect('mongodb+srv://noumedem:jQRA07xoufw3zNB4@cluster0.9zoopoa.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use(express.json())
 
